973bfa40612b358302ba66d820fa88d875a6a2d0
eagle3y3 <hairston.lester@yahoo.com>
2018-12-11 03:59:03 -0600

    Updated json features. Also deleted some unnecessary libs.

--- Web-Crawler/Adidas_Web_Crawler.py
+++ Web-Crawler/Adidas_Web_Crawler.py
@@ -9,12 +9,11 @@ import time
 from bs4 import BeautifulSoup as bs
 import json
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-from cssselect.parser import Class
+
 
  
 
